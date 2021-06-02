d148c2cd6538cb9f7eebbd3783da2cce8d956815
Brendan O\'Daly <odalybr@hotmail.com>
2021-06-02 23:11:46 +0100

    Now sends email from message-us using nativescript-email plugin.

--- src/app/information/message-us/message-us.component.ts
+++ src/app/information/message-us/message-us.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Button, EventData } from '@nativescript/core';
 import { Router } from '../../common';
 
+import * as email from "nativescript-email";
+
 import { LoggerService } from '@src/app/services/logger.service';
 
 @Component({
@@ -12,10 +14,12 @@ import { LoggerService } from '@src/app/services/logger.service';
 export class MessageUsComponent implements OnInit {
   componentName = 'MessageUsComponent';
   logdepth      = 3;
-  public recipient: string = 'secretary@avenueunited.ie';
-  email: string ;
+  // public recipient: string = 'secretary@avenueunited.ie';
+  recipient: string = 'odalybr@hotmail.com';
+  sender: string ;
   subject: string;
   message: string;
+  composeOptions: email.ComposeOptions;
   
   constructor(  private lg$    : LoggerService,
                 private router$: Router ) {
@@ -28,9 +32,33 @@ export class MessageUsComponent implements OnInit {
   {
     this.lg$.log("-> sendEmaiil()");
 
-    this.lg$.log("    email address: " + this.email);
+    this.lg$.log("    email address: " + this.sender);
     this.lg$.log("    Subject: " + this.subject);
     this.lg$.log("    Message: " + this.message);
+    this.lg$.log("    sending email message to " + this.recipient);
+
+    email.available().then(available => {
+      this.lg$.log("        The device email status is $(available)");
+
+      if( available )
+      {
+        this.composeOptions = {
+          to: [this.recipient],
+          subject: this.subject,
+          body: this.message,
+          cc: [this.sender]
+        }
+        email.compose(this.composeOptions).then(result => {
+          this.lg$.log(result);
+          if( result ) {
+            this.lg$.log("Email sent successfully.");
+          }
+          else {
+            this.lg$.error("Email not sent!");
+          }
+        }).catch(error => this.lg$.error(error));
+      }
+    }).catch(error => this.lg$.error(error));
 
     this.lg$.log("    Navigating to home");
     this.lg$.log("<- sendEmaiil()");
@@ -41,7 +69,7 @@ export class MessageUsComponent implements OnInit {
   onButtonTap(args: EventData) {
     const button = <Button>args.object;
     console.log('Button tapped');
-    this.lg$.log("    email address: " + this.email);
+    this.lg$.log("    email address: " + this.sender);
   }
 
 }
